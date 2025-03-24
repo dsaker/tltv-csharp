755152722afe9d10d1db62ec0bbc4ecfd0be9477
Dusty Saker <dustysaker@gmail.com>
2025-03-23 19:10:51 -0500

    add search titles

---
+++ code/TalkLikeTv/TalkLikeTv.Mvc/Models/SearchTitlesViewModel.cs
@@ -0,0 +1,14 @@
+using TalkLikeTv.EntityModels;
+
+namespace TalkLikeTv.Mvc.Models;
+
+public class SearchTitlesViewModel
+{
+    public int? OriginalLanguageId { get; set; }
+    public string? Keyword { get; set; }
+    public IEnumerable<Title>? Results { get; set; }
+    public IEnumerable<Language>? TitleLanguages { get; set; }
+    public string SearchType { get; set; } = "Language";
+    public int PageNumber { get; set; } = 1;
+    public int TotalPages { get; set; }
+}
